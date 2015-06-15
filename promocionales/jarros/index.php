49734ff2b7ebcdab8eca9897c7ec05a69c808998
Ana <anabelengyl@gmail.com>
2015-06-15 16:59:35 -0300

    removed <br> after every form field

---
+++ promocionales/jarros/index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>G&L Enterprise- Jarros Publicitarios </title>
+		<link href='http://fonts.googleapis.com/css?family=Bitter:400,700|Open+Sans:300,600' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="../css/stylefullwidth.css">
+		<meta name="viewport" content="initial-scale=1">
+	</head>
+	<body>
+		<header>
+			<div class="left">
+				<img src="../img/logo.png">
+				<h1>Jarros Publicitarios</h1>
+				<h2>¡Somos fabricantes!</h2>
+			</div>
+			<div class="right">
+				<h1>4115-6008</h1>
+				<h2>¡Llamanos!</h2>
+			</div>
+		</header>
+		<nav>
+			<ul>
+				<li><a href="http://www.gylenterprise.com.ar" target="_blank">Página Principal</a></li>
+				<li><a href="http://www.gylenterprise.com.ar/contacto.php" target="_blank">Dónde estamos</a></li>
+				<li><a href="http://www.gylenterprise.com.ar/empresa.php" target="_blank">Quienes Somos</a></li>
+			</ul>			
+		</nav>
+		<div id="main">
+			<div>
+				<img src="jarros.jpg">
+			</div>
+			<div>
+				<form action="verify.php" method="post">
+					<h2>Contactános</h2>
+					<p>Respondemos presupuestos en el día</p>
+					<input type="text" name="name" id="name" placeholder="Nombre" required>
+					<input type="tel" name="tel" id="tel" placeholder="Teléfono" required>
+					<input type="email" name="email" id="email" placeholder="Email" required>
+					<input type="text" name="mensaje" id="mensaje" placeholder="Escriba su consulta" required>
+					<label>Quiero recibir novedades por mail</label><input type="checkbox" name="newsletter" id="newsletter" value="quiere newsletter" checked="checked"><br>
+					<input type="submit" name="submit" value="Enviar" id="submit-button"><br>
+					<a href="http://www.gylenterprise.com.ar/politicas.php" target="_blank">Al enviar este formulario usted acepta las políticas de privacidad. Click aquí para leerlas</a>
+				</form>
+			</div>
+		</div>
+		<div id="second">
+			<div>
+				<h3 class="variedad">Variedad de productos</h3>
+				<p>Tenemos una amplia gama de productos. Envíanos tu consulta.</p>
+			</div>
+			<div>
+				<h3 class="envios">Hacemos envíos</h3>
+				<p>Envíos a todo el país.<br>Consultanos.</p>
+			</div>
+			<div>
+				<h3 class="confecciones">Apto para estampar</h3>
+				<p>Tenemos jarros sublimados y aptos para estampas vitrificables. Mandános tu consulta.</p>
+			</div>
+		</div>
+		<footer>
+			<p>Av. Caseros 2056 | Cp.: C1264AAN | Tel.: 4115-6008/ 4305-1113  |<a href="mailto:ventas@gylenterprise.com.ar"> Click para enviar un mail</a></p>
+		</footer>
+		<!-- Codigo de remarketing -->
+		<script type="text/javascript">
+		/* <![CDATA[ */
+		var google_conversion_id = 997668516;
+		var google_custom_params = window.google_tag_params;
+		var google_remarketing_only = true;
+		/* ]]> */
+		</script>
+		<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+		</script>
+		<noscript>
+		<div style="display:inline;">
+		<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/997668516/?value=0&amp;guid=ON&amp;script=0"/>
+		</div>
+		</noscript>
+		<!-- Codigo de remarketing -->
+	</body>
+</html>
