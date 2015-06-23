6d2702ea5e57188395b6af9d22c7f5ff3f03b2ff
Ana <anabelengyl@gmail.com>
2015-06-23 12:36:09 -0300

    Contactengine and thanks

---
+++ promocionales/remeras/index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>G&L Enterprise - Remeras Publicitarias </title>
+		<link href='http://fonts.googleapis.com/css?family=Bitter:400,700|Open+Sans:300,600' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="../css/stylefullwidth.css">
+		<meta name="viewport" content="initial-scale=1">
+	</head>
+	<body>
+		<header>
+			<div class="left">
+				<img src="../img/logo.png">
+				<h1>Remeras Publicitarias</h1>
+				<h2>¡Somos fabricantes!</h2>
+			</div>
+			<div class="right">
+				<h1>4115-6008</h1>
+				<h2>¡Llamanos!</h2>
+			</div>
+		</header>
+		<nav>
+			<ul>
+				<li><a href="http://www.gylenterprise.com.ar" target="_blank">Página Principal</a></li>
+				<li><a href="http://www.gylenterprise.com.ar/contacto.php" target="_blank">Dónde estamos</a></li>
+				<li><a href="http://www.gylenterprise.com.ar/empresa.php" target="_blank">Quienes Somos</a></li>
+			</ul>			
+		</nav>
+		<div id="main">
+			<div>
+				<img src="remeras.jpg">
+			</div>
+			<div>
+				<form action="verifyre.php" method="post">
+					<h2>Contactános</h2>
+					<p>Respondemos presupuestos en el día</p>
+					<input type="text" name="name" id="name" placeholder="Nombre" required>
+					<input type="tel" name="tel" id="tel" placeholder="Teléfono" required>
+					<input type="email" name="email" id="email" placeholder="Email" required>
+					<input type="text" name="mensaje" id="mensaje" placeholder="Escriba su consulta" required>
+					<label>Quiero recibir novedades por mail</label><input type="checkbox" name="newsletter" id="newsletter" value="quiere newsletter" checked="checked"><br>
+					<input type="submit" name="submit" value="Enviar" id="submit-button"><br>
+					<a href="http://www.gylenterprise.com.ar/politicas.php" target="_blank">Al enviar este formulario usted acepta las políticas de privacidad. Click aquí para leerlas</a>
+				</form>
+			</div>
+		</div>
+		<div id="second">
+			<div>
+				<h3 class="variedad">Remeras Lisas o impresas con logos</h3>
+				<p>Fabricamos remeras aptas para estampar en serigrafia, sublimar o bordar. También ofrecemos el servicio de estampado. Consultanos</p>
+			</div>
+			<div>
+				<h3 class="envios">Hacemos envíos</h3>
+				<p>Envíamos productos a todo el país<br>Consulte ahora</p>
+			</div>
+			<div>
+				<h3 class="confecciones">Confecciones especiales</h3>
+				<p>Fabricamos remeras en modelos y talles especiales. Contamos con una amplia gama de telas y colores.</p>
+			</div>
+		</div>
+		<div id="third">
+			<div>
+				<h3 class="variedad">Descuentos a mayoristas</h3>
+				<p>Ofrecemos descuentos de 10% a mayoristas. Consultanos</p>
+			</div>
+			<div>
+				<h3 class="envios">Entrega Inmediata</h3>
+				<p>Contamos con stock permanente de nuestros productos de línea. Llévese su pedido en el día<br>Consulte ahora</p>
+			</div>
+			<div>
+				<h3 class="confecciones">Somos líderes en el mercado</h3>
+				<p>Ofrecemos productos de calidad superior y una atención rápida y personalizada.<br>Envíe su consulta o llamenos.  </p>
+			</div>
+		</div>
+		<footer>
+			<p>Av. Caseros 2056 | Cp.: C1264AAN | Tel.: 4115-6008/ 4305-1113  |<a href="mailto:ventas@gylenterprise.com.ar"> Click para enviar un mail</a></p>
+		</footer>
+		<!-- Codigo de remarketing -->
+		<script type="text/javascript">
+		/* <![CDATA[ */
+		var google_conversion_id = 997668516;
+		var google_custom_params = window.google_tag_params;
+		var google_remarketing_only = true;
+		/* ]]> */
+		</script>
+		<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+		</script>
+		<noscript>
+		<div style="display:inline;">
+		<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/997668516/?value=0&amp;guid=ON&amp;script=0"/>
+		</div>
+		</noscript>
+		<!-- Codigo de remarketing -->
+	</body>
+</html>
---
+++ promocionales/remeras/verifyre.php
@@ -0,0 +1,74 @@
+ <?php
+ 	
+		$EmailFrom = "landingremeras@example.com";
+		$EmailFromRe = "noreply@gylenterprise.com.ar";
+		$EmailTo = "ana.belen@gylenterprise.com.ar";
+		$Subject = "Contacto por remeras";
+		$SubjectRe= "Gracias por contactarse con GYL Enterprise";
+		$Name = Trim(stripslashes($_POST['name'])); 
+		$Tel = Trim(stripslashes($_POST['tel'])); 
+		$Email = Trim(stripslashes($_POST['email'])); 
+		$Message = Trim(stripslashes($_POST['mensaje'])); 
+		if(isset($_POST['newsletter'])==false){
+					// Si la función devuelve false, esta variable no existe,
+					// por lo que no se tildó "Newsletter" en el otro archivo.
+					$newsletter = "No quiere newsletter :(";
+					} else {
+					$newsletter = "El cliente si quiere suscribirse al newsletter. \n mail enviado desde http://www.gylenterprise.com.ar/promocionales/remeras ";
+					// para insertar query agregar los datos de conexion en el encabezado.
+					}
+		// validation
+		$validationOK=true;
+		if (!$validationOK) {
+		  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+		  exit;
+		}
+
+		// prepare email body text
+		$Body = "";
+		$Body .= "Nombre: ";
+		$Body .= $Name;
+		$Body .= "\r\n";
+		$Body .= "Tel: ";
+		$Body .= $Tel;
+		$Body .= "\r\n";
+		$Body .= "Email: ";
+		$Body .= $Email;
+		$Body .= "\r\n";
+		$Body .= $newsletter;
+		$Body .= "\r\n";
+		$Body .= "Mensaje: ";
+		$Body .= $Message;
+		$Body .= "\r\n";
+		
+		// email body para el cliente
+		$BodyRe ="Gracias por contactarnos \r\n Alguien de nuestro equipo estará respondiendo su consulta. 
+					\r\n No dude en llamarnos al 4115-6008
+					\r\n Atentamente:
+					\r\n G&L Enterprise
+					\r\n Av. Caseros 2056, Ciudad Autónoma de Buenos Aires";
+		
+		//headers para el mail que llega a la empresa
+		$headers = 'From: ' . $EmailFrom  . "\r\n" .
+				'Content-Type: text/plain; charset=UTF-8' .  "\r\n" .
+				'Reply-To: ' . $EmailFrom  . "\r\n" .
+				'X-Mailer: PHP/' . phpversion();
+		
+		//headers para el mail que llega al cliente
+		$headersRe = 'From: ' . $EmailFromRe  . "\r\n" .
+				'Content-Type: text/plain; charset=UTF-8' .  "\r\n" .
+				'Reply-To: ' . $EmailFromRe  . "\r\n" .
+				'X-Mailer: PHP/' . phpversion();		
+
+		// send email 
+		$success = mail($EmailTo, $Subject, $Body, $headers);
+
+		// redirect to success page y enviar mail al cliente
+		if ($success){
+		  print "<meta http-equiv=\"refresh\" content=\"0;URL=gracias.html\">";
+		  mail($Email, $SubjectRe, $BodyRe, $headersRe);
+		}
+		else{
+		  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+		}
+  ?>
