f74adceb1c0d0e37a7ad1e65a1cedc16a78ba9db
Ana <anabelengyl@gmail.com>
2015-07-20 16:10:08 -0300

    avances en la grilla. Colores de prueba. primeros cambios tipográficos

--- promocionales/remeras/index.php
+++ promocionales/remeras/index.php
@@ -13,18 +13,19 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 		<!-- other sheets -->
 		<link href='http://fonts.googleapis.com/css?family=Bitter:400,700|Open+Sans:300,600' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="../css/styleboot.css">
 	</head>
-	<body class="container-fluid">
+	<body class="container-fluid color4">
 		<nav>
 			<ul class="row">
-				<li class="col-lg-4"><a href="http://www.gylenterprise.com.ar" target="_blank" >Página Principal</a></li>
-				<li class="col-lg-4"><a href="http://www.gylenterprise.com.ar/contacto.php" target="_blank" >Dónde estamos</a></li>
-				<li class="col-lg-4"><a href="http://www.gylenterprise.com.ar/empresa.php" target="_blank" class="col-lg-4">Quienes Somos</a></li>
+				<li class="col-lg-4 color2"><a href="http://www.gylenterprise.com.ar" target="_blank" >Página Principal</a></li>
+				<li class="col-lg-4 color1"><a href="http://www.gylenterprise.com.ar/contacto.php" target="_blank" >Dónde estamos</a></li>
+				<li class="col-lg-4 color2"><a href="http://www.gylenterprise.com.ar/empresa.php" target="_blank" class="col-lg-4">Quienes Somos</a></li>
 			</ul>			
 		</nav>
 		
-		<header class="row">
-			<div class="col-lg-8">
+		<header class="row color2">
+			<div class="col-lg-8 color4">
 				<div class="row">
 					<img src="../img/logo.png" class="col-lg-2">
 					<h1 class="col-lg-6">Remeras Publicitarias</h1>
@@ -42,32 +43,44 @@
 					<div class="producto col-lg-4">
 					<!--Primer remera-->
 						<img src="prod.png">
+						<h3>Remera modelo XXX</h3>
+						<p>texto descriptivo</p>
 					</div>
 					<div class="producto col-lg-4">
 					<!--Segunda remera-->
 						<img src="prod.png">
+						<h3>Remera modelo XXX</h3>
+						<p>texto descriptivo</p>
 					</div>
 					<div class="producto col-lg-4">
 					<!--Tercera remera-->
 						<img src="prod.png">
+						<h3>Remera modelo XXX</h3>
+						<p>texto descriptivo</p>
 					</div>
 				</div>
 				<div class ="row">
 					<div class="producto col-lg-4">
 					<!--Cuarta remera-->
 						<img src="prod.png">
+						<h3>Remera modelo XXX</h3>
+						<p>texto descriptivo</p>
 					</div>
 					<div class="producto col-lg-4">
 					<!--Quinta remera-->
 						<img src="prod.png">
+						<h3>Remera modelo XXX</h3>
+						<p>texto descriptivo</p>
 					</div>
 					<div class="producto col-lg-4">
 					<!--Sexta remera-->	
 						<img src="prod.png">
+						<h3>Remera modelo XXX</h3>
+						<p>texto descriptivo</p>
 					</div>
 				</div>
 			</div>
-			<form action="verifyre.php" method="post" class="col-lg-4">
+			<form action="verifyre.php" method="post" class="col-lg-4 color2">
 					<br>
 					<h2>Contactános</h2>
 					<p>Respondemos presupuestos en el día</p>
@@ -78,11 +91,10 @@
 					<label>Quiero recibir novedades por mail</label><input type="checkbox" name="newsletter" id="newsletter" value="quiere newsletter" checked="checked"><br>
 					<input type="submit" name="submit" value="Enviar" id="submit-button"><br>
 					<a href="http://www.gylenterprise.com.ar/politicas.php" target="_blank">Al enviar este formulario usted acepta las políticas de privacidad. Click aquí para leerlas</a>
-					
 			</form>	
 		</main>
 		
-		<div id="second" class="row">
+		<div id="second" class="row color2">
 			<div class="col-lg-4">
 				<h3 class="variedad">Remeras Lisas o con logos</h3>
 				<p>Fabricamos remeras aptas para estampar en serigrafia, sublimar o bordar. También ofrecemos el servicio de estampado. Consultanos</p>
@@ -110,7 +122,7 @@
 				<p>Ofrecemos productos de calidad superior y una atención rápida y personalizada.<br>Envíe su consulta o llamenos.  </p>
 			</div>
 		</div>
-		<footer class="row">
+		<footer class="row color2">
 			<p>Av. Caseros 2056 | Cp.: C1264AAN | Tel.: 4115-6008/ 4305-1113  |<a href="mailto:ventas@gylenterprise.com.ar"> Click para enviar un mail</a></p>
 		</footer>
 		<!-- Codigo de remarketing -->
