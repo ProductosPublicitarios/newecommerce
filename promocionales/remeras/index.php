e7e4fabf5a0e2e1b525900c86d93abceb3206b04
Ana <anabelengyl@gmail.com>
2015-07-20 13:22:15 -0300

    bootstrap grid

--- promocionales/remeras/index.php
+++ promocionales/remeras/index.php
@@ -3,35 +3,72 @@
 	<head>
 		<meta charset="utf-8">
 		<title>G&L Enterprise - Remeras Publicitarias </title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		
+		<!-- Bootstrap -->
+ 		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+		<!-- other sheets -->
 		<link href='http://fonts.googleapis.com/css?family=Bitter:400,700|Open+Sans:300,600' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="../css/stylefullwidth.css">
-		<meta name="viewport" content="initial-scale=1">
 	</head>
-	<body>
-		<header>
-			<div class="left">
-				<img src="../img/logo.png">
-				<h1>Remeras Publicitarias</h1>
-				<h2>¡Somos fabricantes!</h2>
-			</div>
-			<div class="right">
-				<!--<h1>4115-6008</h1>
-				<h2>¡Llamanos!</h2>-->
-			</div>
-		</header>
+	<body class="container-fluid">
 		<nav>
-			<ul>
-				<li><a href="http://www.gylenterprise.com.ar" target="_blank">Página Principal</a></li>
-				<li><a href="http://www.gylenterprise.com.ar/contacto.php" target="_blank">Dónde estamos</a></li>
-				<li><a href="http://www.gylenterprise.com.ar/empresa.php" target="_blank">Quienes Somos</a></li>
+			<ul class="row">
+				<li class="col-lg-4"><a href="http://www.gylenterprise.com.ar" target="_blank" >Página Principal</a></li>
+				<li class="col-lg-4"><a href="http://www.gylenterprise.com.ar/contacto.php" target="_blank" >Dónde estamos</a></li>
+				<li class="col-lg-4"><a href="http://www.gylenterprise.com.ar/empresa.php" target="_blank" class="col-lg-4">Quienes Somos</a></li>
 			</ul>			
 		</nav>
-		<div id="main">
-			<div>
-				<img src="remeras.jpg">
+		
+		<header class="row">
+			<div class="col-lg-8">
+				<div class="row">
+					<img src="../img/logo.png" class="col-lg-2">
+					<h1 class="col-lg-6">Remeras Publicitarias</h1>
+					<h2 class="col-lg-6">¡Somos fabricantes!</h2>
+				</div>
 			</div>
-			<div>
-				<form action="verifyre.php" method="post">
+			<div class="col-lg-4">
+				<h2>4115-6008 ¡Llamanos!</h2>
+			</div>
+		</header>
+		
+		<main class="row">
+			<div class="col-lg-8">
+				<div class="row">
+					<div class="producto col-lg-4">
+					<!--Primer remera-->
+						<img src="prod.png">
+					</div>
+					<div class="producto col-lg-4">
+					<!--Segunda remera-->
+						<img src="prod.png">
+					</div>
+					<div class="producto col-lg-4">
+					<!--Tercera remera-->
+						<img src="prod.png">
+					</div>
+				</div>
+				<div class ="row">
+					<div class="producto col-lg-4">
+					<!--Cuarta remera-->
+						<img src="prod.png">
+					</div>
+					<div class="producto col-lg-4">
+					<!--Quinta remera-->
+						<img src="prod.png">
+					</div>
+					<div class="producto col-lg-4">
+					<!--Sexta remera-->	
+						<img src="prod.png">
+					</div>
+				</div>
+			</div>
+			<form action="verifyre.php" method="post" class="col-lg-4">
+					<br>
 					<h2>Contactános</h2>
 					<p>Respondemos presupuestos en el día</p>
 					<input type="text" name="name" id="name" placeholder="Nombre" required>
@@ -41,40 +78,39 @@
 					<label>Quiero recibir novedades por mail</label><input type="checkbox" name="newsletter" id="newsletter" value="quiere newsletter" checked="checked"><br>
 					<input type="submit" name="submit" value="Enviar" id="submit-button"><br>
 					<a href="http://www.gylenterprise.com.ar/politicas.php" target="_blank">Al enviar este formulario usted acepta las políticas de privacidad. Click aquí para leerlas</a>
-					<h1>4115-6008</h1>
-					<h2>¡Llamanos!</h2>
-				</form>
-			</div>
-		</div>
-		<div id="second">
-			<div>
+					
+			</form>	
+		</main>
+		
+		<div id="second" class="row">
+			<div class="col-lg-4">
 				<h3 class="variedad">Remeras Lisas o con logos</h3>
 				<p>Fabricamos remeras aptas para estampar en serigrafia, sublimar o bordar. También ofrecemos el servicio de estampado. Consultanos</p>
 			</div>
-			<div>
+			<div class="col-lg-4">
 				<h3 class="envios">Hacemos envíos</h3>
 				<p>Envíamos productos a todo el país<br>Consulte ahora</p>
 			</div>
-			<div>
+			<div class="col-lg-4">
 				<h3 class="confecciones">Confecciones especiales</h3>
 				<p>Fabricamos remeras en modelos y talles especiales. Contamos con una amplia gama de telas y colores.</p>
 			</div>
 		</div>
-		<div id="third">
-			<div>
+		<div id="third" class="row">
+			<div class="col-lg-4">
 				<h3 class="variedad">Descuentos a mayoristas</h3>
 				<p>Ofrecemos descuentos de 10% a mayoristas. Consultanos</p>
 			</div>
-			<div>
+			<div class="col-lg-4">
 				<h3 class="envios">Entrega Inmediata</h3>
 				<p>Contamos con stock permanente de nuestros productos de línea. Llévese su pedido en el día<br>Consulte ahora</p>
 			</div>
-			<div>
+			<div class="col-lg-4">
 				<h3 class="confecciones">Somos líderes en el mercado</h3>
 				<p>Ofrecemos productos de calidad superior y una atención rápida y personalizada.<br>Envíe su consulta o llamenos.  </p>
 			</div>
 		</div>
-		<footer>
+		<footer class="row">
 			<p>Av. Caseros 2056 | Cp.: C1264AAN | Tel.: 4115-6008/ 4305-1113  |<a href="mailto:ventas@gylenterprise.com.ar"> Click para enviar un mail</a></p>
 		</footer>
 		<!-- Codigo de remarketing -->
